13c9b239e44dfd5906e93100c2828c6761a30eb8
sukanyagovindan <gsukanya@gmail.com>
2022-07-09 16:58:47 -0700

    commit tree 00b6e5def715ef2627c2fd6e7325a660778f3f01

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(someArray,someString) {
+    let matches = someArray.filter(item => (item.toLowerCase() === someString.toLowerCase()));
+   return matches;
+}
+
+
+function fuzzyMatch(someArray,beginningLetters) {
+    let matches = someArray.filter(item => (item.indexOf(beginningLetters) == 0));
+    return matches;
+
+}
+
+function matchName(someObject,someString) {
+        let matches = someObject.filter(item => (item.name === someString));
+        return matches;
+    
+    
+}
+
